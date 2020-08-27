25c56ac69bb8fd352be244858075595cd43d6df3
mbersot <marcusbersot@gmail.com>
2020-08-27 19:25:31 -0300

    Front cadastro

---
+++ src/views/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home(){
+    return(
+      <div className="jumbotron">
+        <h1 className="display-3">Bem Vindo!</h1>
+        <p className="lead">
+            Sistema e cadastro de produtos, Utilize a barra de navegação
+        </p>
+        <hr className="my-4" /> 
+        <p className="lead"> 
+            <a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+        </p>
+      </div>  
+    )
+
+}
+
+export default Home
