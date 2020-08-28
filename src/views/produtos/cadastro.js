8f9b74691d5c9910f069cae7b497ce007d9a90a9
mbersot <marcusbersot@gmail.com>
2020-08-27 22:18:34 -0300

    Form Cadastro produto

---
+++ src/views/produtos/cadastro.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const estadoInicial = {
+        nome:'',
+        sku: '',
+        descricao: '',
+        preco: '0',
+        fornecedor: ''
+}
+
+
+class CadastroProduto extends React.Component{
+    state = estadoInicial;
+
+    limpaCampos = () => {
+        this.setState(estadoInicial)
+    }
+    
+    onChange = (event) =>{
+        const valor = event.target.value
+        const nomeDoCampo = event.target.name
+        this.setState({ [nomeDoCampo]: valor })
+    }
+
+    onSubmit = (event) =>{
+        console.log(this.state)
+    }
+
+    
+    render(){
+        return(
+          <div className="card">
+            <div class="card-header">
+                Cadastro de Produto
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>Nome: *</label>
+                        <input type="text" name="nome" onChange={this.onChange} value={this.state.nome} className="form-control" />
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>SKU: *</label>
+                        <input type="text" name="sku" onChange={this.onChange} value={this.state.sku} className="form-control" />
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="form-group">
+                        <label>Descrição:</label>
+                        <textarea name="descricao" onChange={this.onChange} value={this.state.descricao} className="form-control" />  
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>Preço: *</label>
+                        <input type="text" name="preco" onChange={this.onChange} value={this.state.preco} className="form-control" />  
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>Fornecedor:</label>
+                        <input type="text" name="fornecedor" onChange={this.onChange} value={this.state.fornecedor} className="form-control" />  
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-1">
+                    <button onClick={this.onSubmit} className="btn btn-success">Salvar</button>
+                </div>
+                <div className="col-md-1">
+                    <button onClick={this.limpaCampos} className="btn btn-primary">Limpar</button>
+                </div>
+            </div>
+
+        </div>
+        )
+       }
+    }
+
+export default CadastroProduto;
